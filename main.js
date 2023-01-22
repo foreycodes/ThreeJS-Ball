6000af7f4e060e41ff9135bc5db305e1ce33a160
eforrester <esfoures@gmail.com>
2023-01-22 13:23:26 -0500

    Resize to the window's dimensions

--- main.js
+++ main.js
@@ -12,19 +12,24 @@ const material = new THREE.MeshStandardMaterial({
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+// Sizes
+const sizes = {
+  width: window.innerWidth, 
+  height: window.innerHeight,
+}
 // Light
 const light = new THREE.PointLight(0xffffff, 1, 100)
 light.position.set(0, 10, 10)
 scene.add(light)
 
 // Camera
-                                      // FOV, aspect ratio
-const camera = new THREE.PerspectiveCamera(45, 800 / 600)
+                                      // FOV, aspect ratio, near clipping point, far clipping point
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 20
 scene.add(camera)
 
 // Renderer
 const canvas = document.querySelector('.webgl'); 
 const renderer = new THREE.WebGLRenderer({canvas})
-renderer.setSize(800, 600)
+renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
