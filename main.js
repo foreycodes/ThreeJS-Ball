32d181271c89ca4154b081d0575e7a318850dd67
eforrester <esfoures@gmail.com>
2023-01-22 14:08:32 -0500

    Animate sphere colour

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'; 
 import './style.css';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import gsap from 'gsap'
 
 // Set up the scene
 const scene = new THREE.Scene();  
@@ -10,6 +11,7 @@ const scene = new THREE.Scene();
 const geometry = new THREE.SphereGeometry (3, 64, 64)
 const material = new THREE.MeshStandardMaterial({
   color: "#00ff83",
+  roughness: 0.5,
 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
@@ -23,6 +25,7 @@ const sizes = {
 // Light
 const light = new THREE.PointLight(0xffffff, 1, 100)
 light.position.set(0, 10, 10)
+light.intensity = 1.5
 scene.add(light)
 
 // Camera
@@ -60,7 +63,38 @@ window.addEventListener('resize', () => {
 // Re-Render the Canvas
 const loop = () => {
   controls.update()
-  renderer.render(scene, camera); 
-  window.requestAnimationFrame(loop); 
+  renderer.render(scene, camera) 
+  window.requestAnimationFrame(loop)
 }
 loop()
+
+// Timeline
+const tl = gsap.timeline({ defaults: { duration: 1 }})
+// scale in the sphere
+tl.fromTo(mesh.scale, { z:0, x:0, y:0 }, { z: 1, x:1, y: 1 })
+// animate the nav
+tl.fromTo('nav', { y: "-100%" }, { y: "0%" })
+tl.fromTo('title', { opacity: 0 }, { opacity: 1 })
+
+// Mouse animation colour
+let mouseDown = false
+let rgb = []; 
+window.addEventListener('mousedown', () => (mouseDown = true))
+window.addEventListener('mouseup', () => (mouseDown = false))
+
+window.addEventListener('mousemove', (e) => {
+  if(mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageY / sizes.height) * 255),
+      150,
+    ]
+  // Animate
+  let newColor = new THREE.Color(`rgb(${rgb.join(",")})`)
+  gsap.to(mesh.material.color, 
+    { r: newColor.r, 
+      g: newColor.g, 
+      b: newColor.b,
+    }) 
+  }
+})
